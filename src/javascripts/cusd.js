cf2c4a509c29834ced26e96a457feba6e5f95db2
Eric Morris <elm232@cornell.edu>
2014-05-03 16:46:07 -0700

    Active link in nav

--- src/javascripts/cusd.js
+++ src/javascripts/cusd.js
@@ -1,3 +1,9 @@
 $(document).foundation();
 
-// document.querySelector('[data-slide]').setAttribute('data-slide', Math.floor(Math.random()*4+1));
+var page = $('[id$="Page"]').attr('id').slice(0,-4);
+
+$('#' + page + 'Nav').addClass('active');
+
+if (page == 'home') {
+  document.querySelector('[data-slide]').setAttribute('data-slide', Math.floor(Math.random()*4+1));
+}
