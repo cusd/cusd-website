dcac1086058d834892b56b6139b0ee4d8f7a29c3
Eric Morris <elm232@cornell.edu>
2014-05-03 12:33:38 -0700

    Prevent JS errors on non-homepage

--- src/javascripts/cusd.js
+++ src/javascripts/cusd.js
@@ -1,3 +1,3 @@
 $(document).foundation();
 
-document.querySelector('[data-slide]').setAttribute('data-slide', Math.floor(Math.random()*4+1));
+// document.querySelector('[data-slide]').setAttribute('data-slide', Math.floor(Math.random()*4+1));
