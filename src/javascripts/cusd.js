a359a1fb5828f38297496385ecdf0cc61baa3f90
Eric Morris <elm232@cornell.edu>
2014-05-03 23:38:48 -0700

    Direct link to openings modal

--- src/javascripts/cusd.js
+++ src/javascripts/cusd.js
@@ -6,4 +6,10 @@ $('#' + page + 'Nav').addClass('active');
 
 if (page == 'home') {
   document.querySelector('[data-slide]').setAttribute('data-slide', Math.floor(Math.random()*4+1));
+} else if (page == 'contribute') {
+  if (window.location.hash == '#openings') {
+    $(document).ready(function(){
+      $('#openingsModal').foundation('reveal', 'open', '/modals/openings.html');
+    });
+  }
 }
