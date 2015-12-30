9978e1e9eb5ed67b2104f02222766883ac3da960
Nikita Dubnov <nikita.dubnov@gmail.com>
2015-12-30 04:17:52 -0500

    responsivity updates

--- source/javascripts/cusd.js
+++ source/javascripts/cusd.js
@@ -33,3 +33,13 @@ $(document).ready(function(){
     }, 1000);
   }
 });
+
+function mobileMenu(){
+  console.log($('.site-header').height());
+  if ($('.site-header').height() < 120){
+    $('.site-header').animate({height: "172px"}, 300);
+  }
+  else {
+    $('.site-header').animate({height: "92px"}, 300);
+  }
+}
